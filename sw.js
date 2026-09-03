dd3e3abfad156e5ec0ab57f2f3251a148ece4b28
msh18993 <msh18993@users.noreply.github.com>
2026-09-03 20:45:31 +0900

    style: improve spacing between meal cards

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'khu-meals-v2';
+const CACHE = 'khu-meals-v3';
 const ASSETS = ['./', './index.html', './data/menu.json', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));

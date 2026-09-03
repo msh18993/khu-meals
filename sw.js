e2abd75c2345d6c41b78d67a9ff8779e25cb8179
msh18993 <msh18993@users.noreply.github.com>
2026-09-03 20:33:30 +0900

    feat: add KHU meal menu prototype

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = 'khu-meals-v1';
+const ASSETS = ['./', './index.html', './data/menu.json', './manifest.webmanifest', './icon.svg'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
+self.addEventListener('fetch', event => {
+  if (event.request.url.endsWith('/data/menu.json')) {
+    event.respondWith(fetch(event.request).then(response => { const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); return response; }).catch(() => caches.match(event.request)));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
